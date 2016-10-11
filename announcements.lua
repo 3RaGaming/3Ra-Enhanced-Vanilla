e6324cabe5d55796a866de38a12a38aa79da846e
StudMuffin <wholikesgravy.questionmark@gmail.com>
2016-10-11 14:17:14 -0600

    Server Console print
    Added console print for player join, leave, and death.

--- announcements.lua
+++ announcements.lua
@@ -30,5 +30,32 @@ local function show_intro(event)
 	end
 end
 
+function player_died(event)
+  game.speed = 1
+  player = event.player_index
+  if game.players[player].name ~= nil then
+    print("[PUPDATE] | "..game.players[player].name.." | died |"..game.players[player].force.name)
+  end
+end
+
+function player_joined(event)
+  game.speed = 1
+  player = event.player_index
+  if game.players[player].name ~= nil then
+    print("[PUPDATE] | "..game.players[player].name.." | join")
+  end
+end
+
+
+function player_left(event)
+  player = event.player_index
+  if game.players[player].name ~= nil then
+    print("[PUPDATE] | "..game.players[player].name.." | leave")
+  end
+end
+
+Event.register(defines.events.on_player_joined_game, on_player_died)
+Event.register(defines.events.on_player_joined_game, player_joined)
+Event.register(defines.events.on_player_left_game, player_left)
 Event.register(defines.events.on_tick, show_announcement)
 Event.register(defines.events.on_player_created, show_intro)
