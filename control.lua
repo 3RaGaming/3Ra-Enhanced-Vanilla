7e694e7b1d561b124923e64c480a49098a3ca2b4
articulating <samsail@live.com.au>
2016-10-12 10:40:01 +1100

    Update control.lua

--- control.lua
+++ control.lua
@@ -1,8 +1,8 @@
---Enhanced Vanilla
---A 3Ra Gaming compilation
+-- Enhanced Vanilla
+-- A 3Ra Gaming compilation
 if not scenario then scenario = {} end
 if not scenario.config then scenario.config = {} end
---config and event must be called first.
+-- config and event must be required first.
 require "config"
 require "locale/utils/event"
 require "locale/utils/admin"
@@ -11,7 +11,8 @@ require "gravestone"
 require "rocket"
 require "locale/utils/undecorator"
 
---Give starting items.
+-- Give player starting items.
+-- @param event on_player_joined event
 function player_joined(event)
   local player = game.players[event.player_index]
   player.insert{name="iron-plate", count=8}
@@ -21,18 +22,22 @@ function player_joined(event)
   player.insert{name="stone-furnace", count = 2}
 end
 
---Give player weapons after they die.
+-- Give player weapons after they respawn.
+-- @param event on_player_respawned event
 function player_respawned(event)
 	local player = game.players[event.player_index]
 	player.insert{name="pistol", count=1}
 	player.insert{name="firearm-magazine", count=10}
 end
 
---Special command for communicating through our custom web-gui
+-- Send a custom message to the server
+-- @param user username to include
+-- @param message message to print
 function server_message(user, message)
 	print("[WEB] "..user..": "..message)
 	game.print("[WEB] "..user..": "..message)
 end
 
+-- Event handlers
 Event.register(defines.events.on_player_created, player_joined)
 Event.register(defines.events.on_player_respawned, player_respawned)
