e22a183a731a4cd6b851399723b57d5a7a4aa3a1
zackman0010 <computerboyzack@gmail.com>
2016-12-14 21:26:18 -0600

    Updating to use new bot interface in utils

--- announcements.lua
+++ announcements.lua
@@ -36,40 +36,6 @@ local function show_intro(event)
 	end
 end
 
-function player_died(event)
-	local player = event.player_index
-	if game.players[player].name ~= nil then
-		print("[PUPDATE] | "..game.players[player].name.." | died")
-	end
-end
-
-function player_respawned(event)
-	local player = event.player_index
-	if game.players[player].name ~= nil then
-		print("[PUPDATE]| "..game.players[player].name.." | respawn")
-	end
-end
-
-function player_joined(event)
-	local player = event.player_index
-	if game.players[player].name ~= nil then
-		print("[PUPDATE]| "..game.players[player].name.." | join | "..game.players[player].force.name) -- Print for human readability
-		print("PLAYER$join," .. player .. "," .. game.players[player].name .. "," .. game.players[player].force.name) -- Print for computer parsing
-	end
-end
-
-function player_left(event)
-	local player = event.player_index
-	if game.players[player].name ~= nil then
-		print("[PUPDATE]| "..game.players[player].name.." | leave | "..game.players[player].force.name) -- Print for human readability
-		print("PLAYER$leave," .. player .. "," .. game.players[player].name .. "," .. game.players[player].force.name) -- Print for computer parsing
-	end
-end
-
 -- Event handlers
-Event.register(defines.events.on_player_died, player_died)
-Event.register(defines.events.on_player_respawned, player_respawned)
-Event.register(defines.events.on_player_joined_game, player_joined)
-Event.register(defines.events.on_player_left_game, player_left)
 Event.register(defines.events.on_tick, show_announcement)
 Event.register(defines.events.on_player_created, show_intro)
