b3de0865052a02dc925d95b7bcd444b8ed1b7e2c
Zachary Sistrunk <computerboyzack@gmail.com>
2017-03-25 15:59:08 -0500

    Replaced Problems Caused By Stupid Lua
    Lua is stupid, enough said.

--- locale/mapsettings/resources.lua
+++ locale/mapsettings/resources.lua
@@ -1,22 +1,16 @@
 require "crosshatch"
 
-Event.register(defines.events.on_chunk_generated, function(event)
-    local crossWidth = global.scenario.config.mapsettings.cross_width
-	local top_left = event.area.left_top
-	local bottom_right = event.area.right_bottom
+function replacex(surface, top, left, bottom, right)
+	local crossWidth = global.scenario.config.mapsettings.cross_width
+	local top_left = {x = left, y = top}
+	local bottom_right = {x = right, y = bottom}
     local replacement
-	if (top_left.x < -crossWidth / 2) then
+	if (top_left.x <= -crossWidth / 2) then
 		replacement = "iron-ore"
-		if (bottom_right.x) > -crossWidth / 2 then bottom_right.x = -crossWidth / 2 end
-	elseif (bottom_right.x > crossWidth / 2) then
+		if (bottom_right.x) > -crossWidth / 2 then bottom_right.x = (-crossWidth / 2) - 1 end
+	elseif (bottom_right.x >= crossWidth / 2) then
 		replacement = "copper-ore"
-		if (top_left.x) > crossWidth / 2 then top_left.x = crossWidth / 2 end
-	elseif (bottom_right.y > crossWidth / 2) then
-		replacement = "coal"
-		if (top_left.y) > crossWidth / 2 then top_left.y = crossWidth / 2 end
-	elseif (top_left.y < -crossWidth / 2) then
-		replacement = "stone"
-		if (bottom_right.y) > -crossWidth / 2 then bottom_right.y = -crossWidth / 2 end
+		if (top_left.x) < crossWidth / 2 then top_left.x = (crossWidth / 2) + 1 end
 	end
 
 	local names = {"iron-ore", "copper-ore", "coal", "stone"}
@@ -24,13 +18,59 @@ Event.register(defines.events.on_chunk_generated, function(event)
 	if replacement then
 		for _,name in pairs(names) do
 			if name ~= replacement then
-				for _,ore in pairs(event.surface.find_entities_filtered{name = name, area = {top_left, bottom_right}}) do
+				for _,ore in pairs(surface.find_entities_filtered{name = name, area = {top_left, bottom_right}}) do
 					local amount = ore.amount
 					local position = ore.position
 					ore.destroy()
-					event.surface.create_entity{name = replacement, position = position, force = "neutral", amount = amount}
+					surface.create_entity{name = replacement, position = position, force = "neutral", amount = amount}
 				end
 			end
 		end
 	end
+end
+
+function replacey(surface, top, left, bottom, right)
+	local crossWidth = global.scenario.config.mapsettings.cross_width
+	local top_left = {x = left, y = top}
+	local bottom_right = {x = right, y = bottom}
+    local replacement
+	if (bottom_right.y >= crossWidth / 2) then
+		replacement = "coal"
+		if (top_left.y) < crossWidth / 2 then top_left.y = (crossWidth / 2) + 1 end
+	elseif (top_left.y <= -crossWidth / 2) then
+		replacement = "stone"
+		if (bottom_right.y) > -crossWidth / 2 then bottom_right.y = (-crossWidth / 2) - 1 end
+	end
+
+	local names = {"iron-ore", "copper-ore", "coal", "stone"}
+
+	if replacement then
+		for _,name in pairs(names) do
+			if name ~= replacement then
+				for _,ore in pairs(surface.find_entities_filtered{name = name, area = {top_left, bottom_right}}) do
+					local amount = ore.amount
+					local position = ore.position
+					ore.destroy()
+					surface.create_entity{name = replacement, position = position, force = "neutral", amount = amount}
+				end
+			end
+		end
+	end
+end
+
+Event.register(defines.events.on_chunk_generated, function(event)
+	local top = event.area.left_top.y
+	local bottom = event.area.right_bottom.y
+	local left = event.area.left_top.x
+	local right = event.area.right_bottom.x
+    replacex(event.surface, top, left, bottom, right)
+	replacey(event.surface, top, left, bottom, right)
+end)
+
+Event.register(defines.events.on_player_created, function(event)
+	local player = game.players[event.player_index]
+	player.print("To the west is only iron.")
+	player.print("To the east is only copper.")
+	player.print("To the north is only stone.")
+	player.print("To the south is only coal.")
 end)
